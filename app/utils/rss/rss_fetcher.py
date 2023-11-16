ab7a2da9282027ddfb8c41843d53b68de5f63e52
Ahmad Adel <ahmadadelzaki@gmail.com>
2023-11-17 01:06:49 +0200

    fix: response handling for RSSFetcher

--- app/utils/rss/rss_fetcher.py
+++ app/utils/rss/rss_fetcher.py
@@ -1,7 +1,54 @@
 import feedparser
 
+from datetime import datetime, timezone
+from feedparser.util import FeedParserDict
+
 class RSSFetcher:
     @classmethod
     def fetch_by_url(cls, url: str) -> dict:
+        """Fetches RSS feed from the URL provided.
+        Calls a validation function on the response.
+
+        Args:
+            url (str): RSS feed URL
+
+        Returns:
+            dict: validated response
+        """
         result = feedparser.parse(url)
-        return result
+        validated_result = cls._validate(result)
+        return validated_result
+
+    @classmethod
+    def _validate(cls, result: FeedParserDict) -> dict:
+        """Check if the feed parsing result is successful and valid
+
+        Args:
+            result (FeedParserDict): returned by `feedparser.parse`
+
+        Raises:
+            Exception: with error message
+
+        Returns:
+            dict: validated result
+        """
+
+        result_dict = dict(result)
+        
+        if result["bozo"] != False:
+            raise Exception("Invalid Feed Response (False Bozo)")
+        
+        if not "status" in result or result["status"] != 200:
+            raise Exception("Invalid Feed Response (Status not 200)")
+
+        # Convert time struct to datetime object
+        updated_time_struct = result_dict["feed"]["updated_parsed"]
+        updated_datetime = datetime(*updated_time_struct[:6], tzinfo=timezone.utc)
+
+        return {
+            "title": result_dict["feed"]["title"],
+            "description": result_dict["feed"]["description"],
+            "updated": updated_datetime,
+            "entries": result["entries"],
+        }
+
