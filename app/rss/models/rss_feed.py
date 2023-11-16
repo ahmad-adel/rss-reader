84c5baf1c2fab9b7a1cc02b6332c9882a7f3ea47
Ahmad Adel <ahmadadelzaki@gmail.com>
2023-11-17 01:10:07 +0200

    fix: rss model changes to accomodate fields

--- app/rss/models/rss_feed.py
+++ app/rss/models/rss_feed.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class RSSFeed(models.Model):
-    title = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, default="")
     failure_counter = models.IntegerField()
-    last_update = models.DateTimeField(null=True)
-    next_update = models.DateTimeField(null=True)
+    updated = models.DateTimeField(null=True)
+    last_fetch = models.DateTimeField(null=True)
+    next_fetch = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.title
